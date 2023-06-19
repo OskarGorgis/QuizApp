f097164dfed4effbcf6f1d66cfaf05f4b19d66d5
OskarGorgis <oskar.gorgis@poczta.onet.pl>
2023-06-19 13:52:25 +0200

    Startowe ustawienia okna aplikacji

--- GUI/MainWindowClass.py
+++ GUI/MainWindowClass.py
@@ -8,3 +8,14 @@ class QuizMainWindow(QtWidgets.QMainWindow):
         # Inicjalizacja obiektu UI z pliku Pythona
         self.ui = GeneratedQuizWindow2.Ui_MainWindow()
         self.ui.setupUi(self)
+
+        #disable adding question part
+        self.ui.question_add.setDisabled(True)
+        self.ui.answer_a_add.setDisabled(True)
+        self.ui.answer_b_add.setDisabled(True)
+        self.ui.answer_c_add.setDisabled(True)
+        self.ui.answer_d_add.setDisabled(True)
+        self.ui.confirm_add.setDisabled(True)
+
+
+
